a3ee0f47b3d0255c0eecf4847b255bb964d70d10
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-22 20:15:25 +0100

    styles nav logo

--- src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
+++ src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
@@ -2,11 +2,17 @@ import { Button, Stack } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./LandingPageNavigation.module.css";
+import navLogo from "../../../assets/nav-logo.png";
 
 function LandingPageNavigation(props) {
     return (
         <nav className={classes["landing-page-nav-container"]}>
-            <div classname={classes["logo-placeholder"]}>LOGO</div>
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <img
+                    src={navLogo}
+                    style={{ width: "160px", marginBottom: "10px" }}
+                />
+            </Link>
             <Stack spacing={2} direction="row">
                 <Link to="/sign-up" style={{ textDecoration: "none" }}>
                     <Button variant="outlined" size="small" sx={{ width: 80 }}>

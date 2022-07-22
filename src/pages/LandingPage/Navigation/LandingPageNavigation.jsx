f36dfb0f662c42abca7b65822717ff600210ca78
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-22 18:04:47 +0100

    updates navbar styles

--- src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
+++ src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
@@ -6,14 +6,15 @@ import classes from "./LandingPageNavigation.module.css";
 function LandingPageNavigation(props) {
     return (
         <nav className={classes["landing-page-nav-container"]}>
+            <div classname={classes["logo-placeholder"]}>LOGO</div>
             <Stack spacing={2} direction="row">
                 <Link to="/sign-up" style={{ textDecoration: "none" }}>
-                    <Button variant="outlined" size="small">
+                    <Button variant="outlined" size="small" sx={{ width: 80 }}>
                         Sign Up
                     </Button>
                 </Link>
                 <Link to="/login" style={{ textDecoration: "none" }}>
-                    <Button variant="contained" size="small">
+                    <Button variant="contained" size="small" sx={{ width: 80 }}>
                         Log in
                     </Button>
                 </Link>

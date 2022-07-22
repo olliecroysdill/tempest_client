6b5afbf7ccf9fb73d509dc511e9b09d67a9daf34
harrisonporter <harrison.porter@sky.uk>
2022-07-22 20:31:42 +0100

    Spiced up navbar (implemented into AppBar material component)

--- src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
+++ src/pages/LandingPage/Navigation/LandingPageNavigation.jsx
@@ -1,24 +1,32 @@
-import { Button, Stack } from "@mui/material";
+import { AppBar, Button, Stack, Toolbar } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./LandingPageNavigation.module.css";
+import navbarlogo from "./../../../assets/navbarlogo.png";
 
 function LandingPageNavigation(props) {
     return (
-        <nav className={classes["landing-page-nav-container"]}>
-            <Stack spacing={2} direction="row">
-                <Link to="/sign-up" style={{ textDecoration: "none" }}>
-                    <Button variant="outlined" size="small">
-                        Sign Up
-                    </Button>
-                </Link>
-                <Link to="/login" style={{ textDecoration: "none" }}>
-                    <Button variant="contained" size="small">
-                        Log in
-                    </Button>
-                </Link>
-            </Stack>
-        </nav>
+        <AppBar position="static" style={{ background: "#FFFFFF" }}>
+            <Toolbar>
+                <div className={classes.leftside}>
+                    <img src={navbarlogo} alt="logo" className={classes.logo} />
+                </div>
+                <section className={classes.rightside}>
+                    <Stack spacing={2} direction="row">
+                        <Link to="/sign-up" style={{ textDecoration: "none" }}>
+                            <Button variant="outlined" size="small">
+                                Sign Up
+                            </Button>
+                        </Link>
+                        <Link to="/login" style={{ textDecoration: "none" }}>
+                            <Button variant="contained" size="small">
+                                Log in
+                            </Button>
+                        </Link>
+                    </Stack>
+                </section>
+            </Toolbar>
+        </AppBar>
     );
 }
 

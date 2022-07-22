a2ce1ac35b91aee621517c5d868a9d99b99b83ad
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-22 16:02:17 +0100

    initial commit getting client from previous repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./layout/Layout";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/sign-up" element={<SignUpPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </Layout>
+    );
+}
+
+export default App;

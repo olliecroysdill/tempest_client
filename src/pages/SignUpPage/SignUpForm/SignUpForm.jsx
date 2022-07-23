6fa19c6452ccd6a4fd61fa91f4c231ee70f9235f
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-23 11:18:49 +0100

    refactors form input event handlers

--- src/pages/SignUpPage/SignUpForm/SignUpForm.jsx
+++ src/pages/SignUpPage/SignUpForm/SignUpForm.jsx
@@ -9,6 +9,16 @@ function SignUpForm() {
         initialState
     );
 
+    function getDispatchEventHandler(dispatchType) {
+        return (e) => {
+            // @ts-ignore (Strange linter error, incorrectly saying this should take one parameter)
+            dispatchGlobalFormState({
+                type: dispatchType,
+                value: e.target.value
+            });
+        };
+    }
+
     return (
         <Stack
             spacing={3}
@@ -16,93 +26,43 @@ function SignUpForm() {
             component="form"
             alignItems="center"
             width={400}
+            padding={3}
         >
             <Stack spacing={2} direction="row">
                 <SignUpFormInputField
                     id="first-name"
                     label="First Name"
                     data={globalFormState.firstName}
-                    onChange={(e) => {
-                        // @ts-ignore - strange lint error, not properly inferring the number of parameters
-                        dispatchGlobalFormState({
-                            type: "UPDATE_FIRST_NAME",
-                            value: e.target.value
-                        });
-                    }}
-                    onBlur={(e) => {
-                        // @ts-ignore
-                        dispatchGlobalFormState({
-                            type: "VALIDATE_FIRST_NAME",
-                            value: e.target.value
-                        });
-                    }}
+                    onChange={getDispatchEventHandler("UPDATE_FIRST_NAME")}
+                    onBlur={getDispatchEventHandler("VALIDATE_FIRST_NAME")}
                 />
                 <SignUpFormInputField
                     id="last-name"
                     label="Last Name"
                     data={globalFormState.lastName}
-                    onChange={(e) => {
-                        // @ts-ignore
-                        dispatchGlobalFormState({
-                            type: "UPDATE_LAST_NAME",
-                            value: e.target.value
-                        });
-                    }}
-                    onBlur={(e) => {
-                        // @ts-ignore
-                        dispatchGlobalFormState({
-                            type: "VALIDATE_LAST_NAME",
-                            value: e.target.value
-                        });
-                    }}
+                    onChange={getDispatchEventHandler("UPDATE_LAST_NAME")}
+                    onBlur={getDispatchEventHandler("VALIDATE_LAST_NAME")}
                 />
             </Stack>
             <SignUpFormInputField
                 id="email"
                 label="Email"
                 data={globalFormState.email}
-                onChange={(e) => {
-                    // @ts-ignore
-                    dispatchGlobalFormState({
-                        type: "UPDATE_EMAIL",
-                        value: e.target.value
-                    });
-                }}
-                onBlur={(e) => {
-                    // @ts-ignore
-                    dispatchGlobalFormState({
-                        type: "VALIDATE_EMAIL",
-                        value: e.target.value
-                    });
-                }}
+                onChange={getDispatchEventHandler("UPDATE_EMAIL")}
+                onBlur={getDispatchEventHandler("VALIDATE_EMAIL")}
             />
             <SignUpFormInputField
                 id="password"
                 label="Password"
                 type="password"
                 data={globalFormState.password}
-                onChange={(e) => {
-                    // @ts-ignore
-                    dispatchGlobalFormState({
-                        type: "UPDATE_PASSWORD",
-                        value: e.target.value
-                    });
-                }}
-                onBlur={(e) => {
-                    // @ts-ignore
-                    dispatchGlobalFormState({
-                        type: "VALIDATE_PASSWORD",
-                        value: e.target.value
-                    });
-                }}
+                onChange={getDispatchEventHandler("UPDATE_PASSWORD")}
+                onBlur={getDispatchEventHandler("VALIDATE_PASSWORD")}
             />
             <Button
                 variant="contained"
                 sx={{ width: "200px" }}
-                onClick={() => {
-                    // @ts-ignore
-                    dispatchGlobalFormState({ type: "VALIDATE_ENTIRE_FORM" });
-                }}
+                onClick={getDispatchEventHandler("VALIDATE_ENTIRE_FORM")}
             >
                 Create Account
             </Button>

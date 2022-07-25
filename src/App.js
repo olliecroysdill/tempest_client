c680ea2de57ee61ad7931a60d25f94297c44de0b
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-25 19:38:52 +0100

    removed unnecessary layout components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./layout/Layout";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 
 function App() {
     return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/sign-up" element={<SignUpPage />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-        </Layout>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+        </Routes>
     );
 }
 

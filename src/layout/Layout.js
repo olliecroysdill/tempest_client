fd3b781f1d2eef28c7749326e20305706cf2a818
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-25 19:24:07 +0100

    adds responsive page margins to html body element

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,9 @@
-import { ClassNames } from "@emotion/react";
 import React from "react";
 import MainNavigation from "./MainNavigation";
-import classes from "./Layout.module.css";
 
 function Layout(props) {
     return (
-        <section className={classes["app-container"]}>
+        <section>
             <MainNavigation />
             <main>{props.children}</main>
         </section>

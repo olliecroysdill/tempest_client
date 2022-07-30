c235e280a28f365a609d78b4682d21ecb31e0e1e
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-30 14:41:15 +0100

    adds basic carousel currently broken

---
+++ src/pages/LandingPage/Carousel/Carousel.jsx
@@ -0,0 +1,87 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import CarouselItem from "./CarouselItem/CarouselItem";
+import Carousel from "react-material-ui-carousel";
+import houseOfDragonImage from "./../../../assets/houseOfDragon.webp";
+import westworldImage from "./../../../assets/Westworld.jpeg";
+
+function LandingPageCarousel() {
+    return (
+        <Carousel
+            sx={{ width: "90%" }}
+            autoPlay={false}
+            animation="slide"
+            duration="1000"
+            navButtonsProps={{
+                // Move the buttons to the bottom. Unsetting top here to override default style.
+                style: {
+                    width: "60px",
+                    height: "100%",
+                    borderRadius: "0px"
+                }
+            }}
+            navButtonsWrapperProps={{
+                // Move the buttons to the bottom. Unsetting top here to override default style.
+                style: {
+                    transform: "translateY(-50%)",
+                    top: 0,
+                    height: "100%"
+                }
+            }}
+        >
+            <Stack direction="row" alignItems="center">
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="barcelona"
+                />
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="paris"
+                />
+                <CarouselItem
+                    image={houseOfDragonImage}
+                    title="House of the Dragon"
+                    location="new york"
+                />
+            </Stack>
+            <Stack direction="row" width="auto" alignItems="center">
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="barcelona"
+                />
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="paris"
+                />
+                <CarouselItem
+                    image={houseOfDragonImage}
+                    title="House of the Dragon"
+                    location="new york"
+                />
+            </Stack>
+            <Stack direction="row" width="auto" alignItems="center">
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="barcelona"
+                />
+                <CarouselItem
+                    image={westworldImage}
+                    title="Westworld"
+                    location="paris"
+                />
+                <CarouselItem
+                    image={houseOfDragonImage}
+                    title="House of the Dragon"
+                    location="new york"
+                />
+            </Stack>
+        </Carousel>
+    );
+}
+
+export default LandingPageCarousel;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Carousel from "./Carousel/Carousel";
 import LandingPageNavigation from "./Navigation/LandingPageNavigation";
 
 function LandingPage() {
     return (
         <>
             <LandingPageNavigation />
+            <Carousel />
         </>
     );
 }

4bf028c72d3a0b74a42ae8b5f602a0187038a9d4
ollie croysdill <oliver.croysdill@sky.uk>
2022-07-31 14:38:43 +0100

    moves film data array into a separate file

---
+++ src/pages/LandingPage/Carousel/CarouselFilmData.js
@@ -0,0 +1,36 @@
+import houseOfDragonImage from "./../../../assets/houseOfDragon.webp";
+import westworldImage from "./../../../assets/Westworld.jpeg";
+import spiderManImage from "./../../../assets/SpiderMan.jpeg";
+
+export const filmDataArray = [
+    {
+        title: "Spiderman No Way Home",
+        location: "London",
+        image: spiderManImage
+    },
+    {
+        title: "Westworld",
+        location: "New York",
+        image: westworldImage
+    },
+    {
+        title: "House of the Dragon",
+        location: "Paris",
+        image: houseOfDragonImage
+    },
+    {
+        title: "Spiderman No Way Home",
+        location: "London",
+        image: spiderManImage
+    },
+    {
+        title: "Westworld",
+        location: "New York",
+        image: westworldImage
+    },
+    {
+        title: "House of the Dragon",
+        location: "Paris",
+        image: houseOfDragonImage
+    }
+];

b38a75fcd1a5ae51b583e98a25e997c8d51b9a93
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-08-04 21:37:30 +0100

    changes to remote backend

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseLink = "http://localhost:8080";
+const baseLink = "http://13.41.220.42:8081";
 
 const server = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL || baseLink

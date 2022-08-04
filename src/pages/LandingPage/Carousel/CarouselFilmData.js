c322393f70276c89090941ca230583c778b1719d
Gianmarco Luppi <gianmarco.luppi@sky.uk>
2022-08-04 15:35:05 +0100

    carousel images

--- src/pages/LandingPage/Carousel/CarouselFilmData.js
+++ src/pages/LandingPage/Carousel/CarouselFilmData.js
@@ -1,66 +1,69 @@
 import houseOfDragonImage from "./../../../assets/houseOfDragon.webp";
 import westworldImage from "./../../../assets/Westworld.jpeg";
 import spiderManImage from "./../../../assets/SpiderMan.jpeg";
+import LotrImage from "./../../../assets/LOTR.jpg";
+import IndyImage from "./../../../assets/indy.jpeg";
+import PjdnImage from "./../../../assets/peopleDoNothing.jpg";
 
 export const filmDataArray = [
     {
         title: "Spiderman No Way Home",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "New York",
+            country: "United States",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "NYC"
         },
         image: spiderManImage
     },
     {
         title: "Westworld",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "Salt Lake City",
+            country: "United States",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "SLC"
         },
         image: westworldImage
     },
     {
         title: "House of the Dragon",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "Dubrovnik",
+            country: "Croatia",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "DBV"
         },
         image: houseOfDragonImage
     },
     {
-        title: "Spiderman No Way Home",
+        title: "Indiana Jones and the Last Crusade",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "Aqaba",
+            country: "Jordan",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "AQJ"
         },
-        image: spiderManImage
+        image: IndyImage
     },
     {
-        title: "Westworld",
+        title: "The Lord of the Rings: The Fellowship of the Ring",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "Auckland",
+            country: "New Zealand",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "AKL"
         },
-        image: westworldImage
+        image: LotrImage
     },
     {
-        title: "House of the Dragon",
+        title: "People Just do Nothing: Big in Japan",
         location: {
-            name: "London",
-            country: "United Kingdom",
+            name: "Tokyo",
+            country: "Japan",
             locationType: "CITY",
-            cityCode: "LON"
+            cityCode: "TYO"
         },
-        image: houseOfDragonImage
+        image: PjdnImage
     }
 ];
